d7226707259d147d500ca4e0eb2b1adc314fc182
DESKTOP-L8HLNGS\Rajasekhar Reddy <rajashekharareddy1995@gmail.com>
2023-01-11 23:28:00 +0530

    vrwv

--- src/Arrays/New_Class.java
+++ src/Arrays/New_Class.java
@@ -4,20 +4,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.FindBy;
 
 public class New_Class {
-	@FindBy WebElement actions;
-	public static void main(String[] args) {
-		
-		By signIn = By.xpath("//*[contains(text(),'Sign in')]");
-		By userName = By.id("identifierId");
-		By next = By.xpath("//*[contains(text(),'Next')]");
-		By Password = By.xpath("//input[@type='password']");
-		try {
-		WebDriver driver = new ChromeDriver();
+	
+	By signIn = By.xpath("//*[contains(text(),'Sign in')]");
+	By userName = By.id("identifierId");
+	By next = By.xpath("//*[contains(text(),'Next')]");
+	By Password = By.xpath("//input[@type='password']");
+	
+	public WebDriver driver;
+	public WebElement actions;
+	public  void navigateToURL() {
+		driver = new ChromeDriver();
 		driver.navigate().to("https://www.google.com");
 		driver.manage().window().maximize();
+		
+		
+		
 		driver.navigate().to("https://www.gmail.com");
 		driver.findElement(signIn).click();
 		WebElement username = driver.findElement(userName);
@@ -28,11 +33,6 @@ public class New_Class {
 		driver.findElement(next).click();
 		driver.quit();
 	
-	}catch(Throwable e) {
-		System.out.println(e.getMessage());
-	}
-		
-
 	}
 }
 
---
+++ src/Arrays/TestingPurpose.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+public class TestingPurpose {
+	
+	public WebDriver driver;
+	
+	public void openBrowser() {
+		driver.findElement(By.xpath(""));
+		
+	}
+
+	
+
+	
+}
